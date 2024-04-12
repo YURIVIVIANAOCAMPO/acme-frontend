5ffc10f98ab58d571dab8e3fca84534c334930dc
Yury Viviana Ocampo <darataz0402@gmail.com>
2024-04-12 13:26:36 -0500

    se crean servicios para la gestión y visualización de usuarios

--- src/app/services/users-login.service.ts
+++ src/app/services/users-login.service.ts
@@ -1,17 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class PruebaService {
+export class UsersLoginService {
   readonly APIUrl = "http://localhost:8000/api/";
   constructor(private http: HttpClient) {
    }
-   login(login:any): Observable<any> {
-    return this.http.get(`${this.APIUrl}users`, login );
-  }
 
-  //usersLogin
+  getAll(){
+    return this.http.get(`${this.APIUrl}users`);
+  } 
+  postUsers(user:any){
+    return this.http.post(`${this.APIUrl}users`,user); 
+  }
 }
--- src/app/users-login/users-login.component.ts
+++ src/app/users-login/users-login.component.ts
@@ -1,23 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { UsersLoginService } from '../services/users-login.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-users-login',
   standalone: true,
-  imports: [],
+  imports: [ FormsModule,],
   templateUrl: './users-login.component.html',
   styleUrl: './users-login.component.css'
 })
-export class UsersLoginComponent {
-/**
- *
- */
-constructor(private modalService:NgbModal) {
+export class UsersLoginComponent implements OnInit {
+
+constructor(
+  private modalService:NgbModal,
+  private UserLoginService:UsersLoginService
+) 
+{}
+ngOnInit(): void {
+  this.getAllUsers();
+}
+dataTable:any;
+  atributUser = {
+    name:'',
+    email:'',
+    password:''
+
+  }
+  getAllUsers()
+  {
+    this.UserLoginService.getAll().subscribe((res:any)=>{
+      console.log(res);
+      this.dataTable=res;
+    })
+  }
+  createUsers()
+  {
+    this.UserLoginService.postUsers(this.atributUser).subscribe((res:any)=>{
+      console.log(res);
+      this.getAllUsers();
+    })
+    this.modalService.dismissAll();
+  }
+
+  open(content:any) {
+    const modalRef = this.modalService.open(content);
+    modalRef.componentInstance.name = 'World';
+  }
   
-  
 }
-open(content:any) {
-  const modalRef = this.modalService.open(content);
-  modalRef.componentInstance.name = 'World';
-}
-}
+
+
+
+
